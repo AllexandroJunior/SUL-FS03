4fc1c4b11e662195364d2983f5026a72bdaaa388
unknown <xandi.jr@live.com>
2023-08-24 22:08:52 -0300

    salvar dia 24/08

---
+++ m2/u1/a06/AllexandroJunior/comparar.mjs
@@ -0,0 +1,14 @@
+// Leia dois numeros do terminal e indentifique se o primeiro
+// é maior que o segundo  
+
+import leitor from '../../../../lib/leitor.mjs'
+
+const numero1 = await leitor('Digite o primeiro número >')
+
+const numero2 = await leitor('Digite o segundo número > ')
+
+if (numero1 > numero2) {
+    console.log('O primeiro número é maior que o segundo')
+}else {
+    console.log('O segundo número é maior que o primeiro')
+}
---
+++ m2/u1/a06/AllexandroJunior/subtracao.mjs
@@ -0,0 +1,9 @@
+import leitor from '../../../../lib/leitor.mjs'
+
+const numero1 = await leitor('Digite o primeiro número >')
+
+const numero2 = await leitor('Digite o segundo número >')
+
+const numero3 = numero1 - numero2
+
+console.log(numero3)
